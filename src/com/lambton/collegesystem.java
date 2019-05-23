0947c08f1ed0e7e12d198881a53c7237b05b4f6b
ujwalarora34 <50931420+ujwalarora34@users.noreply.github.com>
2019-05-23 19:42:10 -0400

    Initial commit

---
+++ src/com/lambton/collegesystem.java
@@ -0,0 +1,11 @@
+package com.lambton;
+
+public class collegesystem
+{
+    public static void main(String[] args)
+    {
+        for (int i=0; i<10; i++)
+        System.out.println("My Name Is Ujwal");
+
+    }
+}
